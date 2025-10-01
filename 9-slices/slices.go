a0aa89d9c4808a44ee0034976cc8ac1a39df05b4
fisayo-dev <olufisayobadina@gmail.com>
2025-10-01 02:14:33 +0100

    chore: create variadic function

--- 9-slices/slices.go
+++ 9-slices/slices.go
@@ -2,11 +2,29 @@ package main
 
 import "fmt"
 
+// Variadic function
+
+func sums(nums ...int) int {
+	num := 0
+	for i := 0; i < len((nums)); i ++ {
+		num += (nums)[i]
+	}
+
+	return num
+}
+
 func main() {
 
-	a := [5]int{6, 3, 5, 6, 7} // An array
-	b := a[0:3] // A slice 
+	// a := [5]int{6, 3, 5, 6, 7} // An array
+	// b := a[0:3] // A slice
 
-	fmt.Println(b)
+	// mySlice := make([]int, 5, 10)
+	// mySlice := []int{3, 4, 5, 6}
+
+	// fmt.Println(mySlice)
+	// fmt.Println("Max length",len(mySlice))
+	// fmt.Println("Max cap",cap(mySlice))
+
+	fmt.Println("Total sum is:",sums(2,4,4,5))
 
 }
