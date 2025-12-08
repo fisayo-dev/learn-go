069623526a64f94ff33a2794ee9bd80e296310a5
fisayo-dev <olufisayobadina@gmail.com>
2025-12-08 15:07:11 +0100

    declare a map

--- 10-maps/maps.go
+++ 10-maps/maps.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type members struct {
+	age int
+	role string
+}
+
 func main() {
-	
+	// declaring a map,
+	fscMembers := map[string]members{
+		"Olema":  {19, "Product Designer"},
+		"Fisayo": {20, "Frontend Developer"},
+		"Divine": {20, "Backend Developer"},
+		"Nifemi": {20, "Project Manager"},
+	}
+
+	fmt.Println(fscMembers["Olema"].age)
 }
