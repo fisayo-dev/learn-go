df2193f98f5ed2cb049aee70356ea6387738044c
fisayo-dev <olufisayobadina@gmail.com>
2025-12-09 14:43:13 +0100

    implement closer to count numbers

--- 11-advanced-func/ad-func.go
+++ 11-advanced-func/ad-func.go
@@ -2,20 +2,41 @@ package main
 
 import "fmt"
 
-func add(x, y int) int {
-	return x + y
-}
+// High order and First-class fund
 
-func multiply(x, y int) int {
-	return x * y
-}
+// func add(x, y int) int {
+// 	return x + y
+// }
 
-func aggregator(a, b, c int, aggregate func(int, int) int) int {
-	return aggregate(aggregate(a, b), c)
+// func multiply(x, y int) int {
+// 	return x * y
+// }
+
+// func aggregator(a, b, c int, aggregate func(int, int) int) int {
+// 	return aggregate(aggregate(a, b), c)
+// }
+
+// Closures
+
+func adder() func (int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	fmt.Println(aggregator(2,3,4, add))
-	fmt.Println(aggregator(2,3,4, multiply))
+
+	fmt.Println("====================")
+	defer fmt.Println("====================")
+	nums := []int{2,3,4,5,5,6,7,7}
+	countAdder, numAdder := adder(), adder()
+	for _, num := range nums {
+		fmt.Printf("The count of the %v numbers is %v \n", countAdder(1), numAdder(num))
+	}
+
+	// fmt.Println(aggregator(2,3,4, add))
+	// fmt.Println(aggregator(2,3,4, multiply))
 
 }
