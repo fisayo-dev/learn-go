e1b75678ce91a1a31f313215b9ef2ccb3994bb35
fisayo-dev <olufisayobadina@gmail.com>
2025-12-09 15:20:37 +0100

    use pointer in methods

--- 12-pointers/pointer.go
+++ 12-pointers/pointer.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	// "strings"
 )
 
 type car struct {
@@ -14,32 +14,33 @@ func (c *car) setColor(color string) {
 }
 
 // Declaring that message is a type of pointer
-func removeProfanity(message *string) {
-	messageVal := *message // Destructuring message to normal string
-	messageVal = strings.ReplaceAll(messageVal, "dang", "****")
-	messageVal = strings.ReplaceAll(messageVal, "heck", "****")
-	messageVal = strings.ReplaceAll(messageVal, "fuck", "****")
-	messageVal = strings.ReplaceAll(messageVal, "shoot", "*****")
-	// Set that memeory address of message to the modified message value
-	*message = messageVal
-}
+// func removeProfanity(message *string) {
+// 	messageVal := *message // Destructuring message to normal string
+// 	messageVal = strings.ReplaceAll(messageVal, "dang", "****")
+// 	messageVal = strings.ReplaceAll(messageVal, "heck", "****")
+// 	messageVal = strings.ReplaceAll(messageVal, "fuck", "****")
+// 	messageVal = strings.ReplaceAll(messageVal, "shoot", "*****")
+// 	// Set that memeory address of message to the modified message value
+// 	*message = messageVal
+// }
 
 
 
 func main() {
-	msg1 := "Shut the fuck up"
-	msg2 := "Men, shoot"
-	msg3 := "What the heck is wrong with you"
-	removeProfanity(&msg1)
-	fmt.Println("Modified words", msg1)
-	removeProfanity(&msg2)
-	fmt.Println("Modified words", msg2)
-	removeProfanity(&msg3)
-	fmt.Println("Modified words", msg3)
+	// msg1 := "Shut the fuck up"
+	// msg2 := "Men, shoot"
+	// msg3 := "What the heck is wrong with you"
+	// removeProfanity(&msg1)
+	// fmt.Println("Modified words", msg1)
+	// removeProfanity(&msg2)
+	// fmt.Println("Modified words", msg2)
+	// removeProfanity(&msg3)
+	// fmt.Println("Modified words", msg3)
 
+	// 
 	myOwnColor := car{"red"}
 	myOwnColor.setColor("blue")
-	println("Color after change:" , myOwnColor.color)
+	fmt.Println("Color after change:" , myOwnColor.color)
 
 }
 
